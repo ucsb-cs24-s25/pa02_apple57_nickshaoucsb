eaff257dcac335bf20fc5b92de29ac31b3d5bbaa
Nicholas Shao <nicholasshao@cs.ucsb.edu>
2025-05-20 19:55:53 -0700

    Early progress

--- movies.cpp
+++ movies.cpp
@@ -1,1 +1,25 @@
 #include "movies.h"
+#include <unordered_map>
+#include <vector>
+using namespace std; 
+
+struct Movie {
+    string title;
+    double rating;
+};
+
+vector<Node> trie(1);
+vector<Movie> Movies; 
+
+struct Node{
+    unordered_map<char,int> next; //An unordered map of the next possible characters following the current one
+    vector<int>  titleIndices; //A vector of integers storing the indices of movies whose title ends at this letter node (indices within global movie vector for fast retrieval)
+};
+
+void insertMovie(const string& MovieTitle, int index){  //int index is the movie's index within global Movies vector
+    int currIndex = 0; //current index within the vector of Nodes Trie
+
+    for(char c:MovieTitle){
+        auto& nextLetters = trie.at(currIndex).next;
+    }
+}
