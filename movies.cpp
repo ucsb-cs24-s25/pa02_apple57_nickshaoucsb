57cda2114c59d789339d846ad1748506428761f6
Nicholas Shao <nicholasshao@cs.ucsb.edu>
2025-05-20 21:50:23 -0700

    Created some functions

--- movies.cpp
+++ movies.cpp
@@ -23,3 +23,29 @@ void MovieList::insert(const string& MovieTitle, int index){  //int index is the
     trie.at(currIndex).titleIndices.push_back(index); //the final node in sequence of characters (the index the final character mapped to) contains the index of the movie in Movies
 }
 
+void accumulateMovies(int index, vector<int>& movieIndexes){ //this function starts from a specified point within Trie (index) and gathers indexes of movies stored within each node's titleIndices
+    movieIndexes.insert(movieIndexes.end(),trie.at(index).titleIndices.begin(),trie.at(index).titleIndices.end());
+
+    for(auto mapping : trie.at(index).next){
+        int nextIndex = mapping.second;
+        accumulateMovies(nextIndex,movieIndexes);
+        return;
+    }
+    return;
+}
+
+int findPrefixIndex(const string& prefix){//this function finds which index within Trie the specified prefix ends
+    int currIndex = 0;
+
+    for(char c:prefix){
+        auto& letterMapping = trie.at(currIndex).next;
+        auto letterTracker = letterMapping.find(c);
+        if(letterTracker==letterMapping.end()){
+            return -1;
+        }
+        else{
+            currIndex = letterTracker->second;
+        }
+    }
+    return currIndex;
+}
